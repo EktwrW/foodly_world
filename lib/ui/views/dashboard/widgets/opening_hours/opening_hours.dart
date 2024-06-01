19422037baf0aa76d8d384471717c2557fd2e0d0
EktwrW <hwaldmann@gmail.com>
2024-06-01 11:33:07 +0100

    feat[opening-hours]: add placeholder

--- lib/ui/views/dashboard/widgets/opening_hours/opening_hours.dart
+++ lib/ui/views/dashboard/widgets/opening_hours/opening_hours.dart
@@ -1,6 +1,11 @@
+import 'package:animate_do/animate_do.dart' show FadeIn;
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foodly_world/generated/l10n.dart';
+import 'package:foodly_world/ui/theme/foodly_text_styles.dart';
+import 'package:foodly_world/ui/views/dashboard/bloc/dashboard_bloc.dart';
 import 'package:foodly_world/ui/views/dashboard/view_model/dashboard_vm.dart';
+import 'package:foodly_world/ui/views/dashboard/widgets/dashboard_save_and_cancel_buttons.dart';
 import 'package:foodly_world/ui/views/dashboard/widgets/dashboard_titles_rich_text.dart';
 
 class OpeningHoursWdg extends StatelessWidget {
@@ -13,9 +18,46 @@ class OpeningHoursWdg extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return DashboardSectionsTitle(
-      firstText: '${S.current.dashboardOpeningHoursText1} ',
-      secondText: S.current.dashboardOpeningHoursText2,
+    final bloc = context.read<DashboardBloc>();
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        DashboardSectionsTitle(
+          firstText: '${S.current.dashboardOpeningHoursText1} ',
+          secondText: S.current.dashboardOpeningHoursText2,
+        ),
+        Visibility(
+          visible: vm.isEditingOpeningHours,
+          replacement: TextButton(
+            onPressed: () {
+              bloc.add(const DashboardEvent.updateEditing(DashboardEditing.openingHours));
+            },
+            child: Row(
+              children: [
+                Expanded(
+                  child: FadeIn(
+                    child: Text(
+                      S.current.addOpeningHours,
+                      style: (vm.currentBusiness?.openingHours != null)
+                          ? FoodlyTextStyles.actionsBody
+                          : FoodlyTextStyles.profileSectionTextButton,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+          child: Form(
+            key: vm.formKey,
+            autovalidateMode: vm.autovalidateMode,
+            child: DashboardSaveAndCancelButtons(
+              showSaveButton: false,
+              onCancelPressed: () => bloc.add(const DashboardEvent.updateEditing(DashboardEditing.none)),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
