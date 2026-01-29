6fc60b02bcc9a0e82dff566eaf76cb349b94c074
Hector Waldmann <hwaldmann@gmail.com>
2026-01-29 09:23:55 +0000

    feat: restore location services with new Google Maps API key
    - Enable Google Maps APIs in GCP (Places, Maps Android/iOS, Geocoding)
    - Create new API key for project beaming-sunset-481614-p3
    - Update API key in Android (AndroidManifest.xml)
    - Update API key in iOS (AppDelegate.swift)
    - Update API key in VSCode configurations (launch.json, tasks.json)
    - New API key: AIzaSyCdFACGaW5-9vVZjnrfYb51MS0Mp3gB5o4

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -8,7 +8,7 @@ import GoogleMaps
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyDQd8kLET9EaWLZH4MeBDLMhsL_sN0RDyY")
+    GMSServices.provideAPIKey("AIzaSyCdFACGaW5-9vVZjnrfYb51MS0Mp3gB5o4")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
