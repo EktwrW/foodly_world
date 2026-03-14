4df355be20509f3107869dbe4bc51921296b4f5d
Hector Waldmann <hwaldmann@gmail.com>
2026-03-14 00:43:55 +0000

    app-scroll fix + recover password localizations

--- lib/core/extensions/app_scroll_extension.dart
+++ lib/core/extensions/app_scroll_extension.dart
@@ -10,4 +10,8 @@ class AppScrollBehavior extends MaterialScrollBehavior {
         PointerDeviceKind.trackpad,
         PointerDeviceKind.unknown,
       };
+
+  // Disable Android stretch overscroll to avoid "_StretchController" build-during-frame assertion
+  @override
+  Widget buildOverscrollIndicator(BuildContext context, Widget child, ScrollableDetails details) => child;
 }
