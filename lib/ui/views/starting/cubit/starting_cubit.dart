d55ebedff01ea9d6ae232cdd0f340c78d3a5fb56
Hector Waldmann <hwaldmann@gmail.com>
2026-05-21 22:34:43 +0100

    removed default test user

--- lib/ui/views/starting/cubit/starting_cubit.dart
+++ lib/ui/views/starting/cubit/starting_cubit.dart
@@ -47,8 +47,10 @@ class StartingCubit extends Cubit<StartingState> {
         _logger = logger,
         _meRepo = meRepo,
         _vm = StartingVM(
-          emailController: TextEditingController(text: 'userdemo1@mail.com'),
-          passwordController: TextEditingController(text: 'Test@123'),
+          emailController: TextEditingController(),
+          passwordController: TextEditingController(),
+          // emailController: TextEditingController(text: 'userdemo1@mail.com'),
+          // passwordController: TextEditingController(text: 'Test@123'),
         ),
         super(const StartingState.initial()) {
     if (kIsWeb) FlutterNativeSplash.remove();
