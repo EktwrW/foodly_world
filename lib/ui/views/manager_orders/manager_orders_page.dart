214a63e7b7ced896ad09126efa7d12e05e42b9bd
Hector Waldmann <hwaldmann@gmail.com>
2026-08-05 10:14:53 +0100

    imports cleanup

--- lib/ui/views/manager_orders/manager_orders_page.dart
+++ lib/ui/views/manager_orders/manager_orders_page.dart
@@ -1,13 +1,7 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:foodly_world/core/core_exports.dart';
-import 'package:foodly_world/core/routing/app_router.dart';
-import 'package:foodly_world/core/services/dependency_injection_service.dart' show di;
-import 'package:foodly_world/generated/l10n.dart';
 import 'package:foodly_world/ui/constants/ui_decorations.dart';
 import 'package:foodly_world/ui/shared_widgets/snackbar/foodly_snackbars.dart';
 import 'package:foodly_world/ui/theme/foodly_text_styles.dart';
-import 'package:foodly_world/ui/theme/foodly_themes.dart';
 import 'package:foodly_world/ui/views/manager_orders/cubit/manager_history_cubit.dart';
 import 'package:foodly_world/ui/views/manager_orders/cubit/manager_orders_cubit.dart';
 import 'package:foodly_world/ui/views/manager_orders/manager_order_detail_page.dart';
