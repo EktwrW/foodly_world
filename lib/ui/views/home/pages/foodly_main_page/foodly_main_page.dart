7dce2f1d342917cf11eb253e437825fc42f69693
Hector Waldmann <hwaldmann@gmail.com>
2026-09-11 14:37:07 +0100

    Etiquetas de sección: 44 px de aire arriba en tableta, no 36
    Ajuste de Héctor probando en la Lenovo. Las etiquetas quedaban apretadas contra
    la sección de arriba con 36; en teléfono no se toca.

--- lib/ui/views/home/pages/foodly_main_page/foodly_main_page.dart
+++ lib/ui/views/home/pages/foodly_main_page/foodly_main_page.dart
@@ -21,7 +21,7 @@ class FoodlyMainPage extends StatelessWidget {
 
   (double, double) _labelsTopAndBottomPadding(BuildContext ctx) {
     if (ctx.isTablet) {
-      return (36, 18);
+      return (44, 18);
     }
     return (26, 12);
   }
